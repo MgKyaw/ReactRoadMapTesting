6a4047008cb846fb6ae45a5cd92738d47471a2ab
MgKyaw <mgkyaw20112@gmail.com>
2026-07-15 23:08:42 +0700

    feat: Add comment at playwright.config.js

--- PlayWright/playwright.config.js
+++ PlayWright/playwright.config.js
@@ -7,3 +7,8 @@ export default defineConfig({
   workers: 4, // runs up to 4 test files in parallel
 
 });
+
+// import { test } from '@playwright/test';
+
+
+// test.describe.configure({ mode: 'parallel' });
