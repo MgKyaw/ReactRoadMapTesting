7769bceafc1513effb8bf820e9aa082084c48098
MgKyaw <mgkyaw20112@gmail.com>
2026-07-18 11:55:15 +0700

    feat: Add the new example test file

--- PlayWright/tests/example.ts
+++ PlayWright/tests/example.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
 
-test('user can navigate to more information page', async ({ page }) => {
+// test('user can navigate to more information page', async ({ page }) => {
 
- await page.goto('https://example.com');
-
-
- await page.getByRole('link', { name: 'More information' }).click();
+//  await page.goto('https://example.com');
 
 
- await expect(page).toHaveURL(/.*iana.org/);
+//  await page.getByRole('link', { name: 'More information' }).click();
 
-});
+
+//  await expect(page).toHaveURL(/.*iana.org/);
+
+// });
