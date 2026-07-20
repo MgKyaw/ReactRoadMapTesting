fba0bd49cc3c4d4685a776bb12be3c52db0061da
MgKyaw <mgkyaw20112@gmail.com>
2026-07-20 22:19:35 +0700

    feat: Add defineConfig.js

---
+++ PlayWright/tests/defineConfig.js
@@ -0,0 +1,13 @@
+export default defineConfig({
+
+  projects: [
+
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+
+  ],
+
+});
