f413a0362cc15d38f0d4bfac98e686f1cbb6573d
MgKyaw <mgkyaw20112@gmail.com>
2026-07-21 19:25:00 +0700

    feat: Add new test to jest

--- react-testing-library/jest.js
+++ react-testing-library/jest.js
@@ -3,3 +3,13 @@ describe('my function or component', () => {
 
   });
 });
+
+describe('true is truthy and false is falsy', () => {
+  it('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false is falsy', () => {
+    expect(false).toBe(false);
+  });
+});
