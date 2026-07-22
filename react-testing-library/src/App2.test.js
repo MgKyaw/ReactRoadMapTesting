65681afc92d8dd3850d7be0dca645e3465f1543d
MgKyaw <mgkyaw20112@gmail.com>
2026-07-22 22:50:33 +0700

    feat: Add more test

--- react-testing-library/src/App2.test.js
+++ react-testing-library/src/App2.test.js
@@ -24,6 +24,26 @@
 //   });
 // });
 
+// import * as React from 'react';
+// import { render, screen } from '@testing-library/react';
+
+// import App from './App';
+
+// describe('App', () => {
+//   it('renders App component', () => {
+//     render(<App />);
+
+//     // implicit assertion
+//     // because getByText would throw error
+//     // if element wouldn't be there
+//     screen.getByText('Search:');
+
+//     // explicit assertion
+//     // recommended
+//     expect(screen.getByText('Search:')).toBeInTheDocument();
+//   });
+// });
+
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
@@ -33,13 +53,13 @@ describe('App', () => {
   it('renders App component', () => {
     render(<App />);
 
-    // implicit assertion
-    // because getByText would throw error
-    // if element wouldn't be there
-    screen.getByText('Search:');
+    // fails
+    expect(screen.getByText('Search')).toBeInTheDocument();
 
-    // explicit assertion
-    // recommended
+    // succeeds
     expect(screen.getByText('Search:')).toBeInTheDocument();
+
+    // succeeds
+    expect(screen.getByText(/Search/)).toBeInTheDocument();
   });
 });
