9008e36320d164e163a6e5e17e9d4ec093e150da
MgKyaw <mgkyaw20112@gmail.com>
2026-07-23 19:45:45 +0700

    feat: Add expect.js

---
+++ react-testing-library/src/expect.js
@@ -0,0 +1,16 @@
+describe('App', () => {
+  it('renders App component', async () => {
+    render(<App />);
+
+    // wait for the user to resolve
+    await screen.findByText(/Signed in as/);
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull();
+
+    fireEvent.change(screen.getByRole('textbox'), {
+      target: { value: 'JavaScript' },
+    });
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
+  });
+});
