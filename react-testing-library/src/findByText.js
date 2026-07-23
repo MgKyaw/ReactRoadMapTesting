e55b9c623bcd15f7a6dfebf46efbe181ad75bcaa
MgKyaw <mgkyaw20112@gmail.com>
2026-07-23 19:43:43 +0700

    feat: Modify findByText.js

--- react-testing-library/src/findByText.js
+++ react-testing-library/src/findByText.js
@@ -1,3 +1,18 @@
+// import * as React from 'react';
+// import { render, screen } from '@testing-library/react';
+
+// import App from './App';
+
+// describe('App', () => {
+//   it('renders App component', async () => {
+//     render(<App />);
+
+//     expect(screen.queryByText(/Signed in as/)).toBeNull();
+
+//     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+//   });
+// });
+
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 
@@ -9,6 +24,10 @@ describe('App', () => {
 
     expect(screen.queryByText(/Signed in as/)).toBeNull();
 
+    screen.debug();
+
     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+
+    screen.debug();
   });
 });
