2f3ddddb3f381d1e0b26c751efa9ad8f65eb13d0
MgKyaw <mgkyaw20112@gmail.com>
2026-07-26 23:57:59 +0700

    feat: Add test result

--- Jest/Testing Javascript with Jest/sample/tests/token.test.js
+++ Jest/Testing Javascript with Jest/sample/tests/token.test.js
@@ -17,3 +17,19 @@ describe("isTokenExpired", () => {
 //// Test command
 
 // npm test -- tests/token.test.js
+
+// Test result
+
+// > sample@1.0.0 test
+//  > jest tests/token.test.js
+
+//  PASS  tests/token.test.js
+//  isTokenExpired
+//      ✓ should return false if the token is not expired (2 ms)
+//      ✓ should return true if the token is expired (1 ms)
+
+//  Test Suites: 1 passed, 1 total
+//  Tests:       2 passed, 2 total
+//  Snapshots:   0 total
+//  Time:        0.315 s, estimated 1 s
+//  Ran all test suites matching /tests\/token.test.js/i.
