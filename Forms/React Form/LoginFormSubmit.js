69f443d3b9726ff1ff035a8d2ada6a1a76064889
MgKyaw <mgkyaw20112@gmail.com>
2026-07-28 20:11:30 +0700

    feat: Add LoginForm Submit

---
+++ Forms/React Form/LoginFormSubmit.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+const LoginForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export { LoginForm };
