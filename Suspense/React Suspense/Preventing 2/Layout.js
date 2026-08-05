950d511cc6192161f28b18c7814acce605d23bef
MgKyaw <mgkyaw20112@gmail.com>
2026-08-05 23:31:32 +0700

    feat: Add Layout.js

---
+++ Suspense/React Suspense/Preventing 2/Layout.js
@@ -0,0 +1,12 @@
+export default function Layout({ children }) {
+  return (
+    <div className="layout">
+      <section className="header">
+        Music Browser
+      </section>
+      <main>
+        {children}
+      </main>
+    </div>
+  );
+}
